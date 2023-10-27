d2a10ddc9ec899bd69ee9a3549ad9a360e6ded01
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-10-27 13:39:09 +0200

    added routes with dynamodb

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,19 @@
 var express = require("express");
 var router = express.Router();
-let content = require("../content.json");
-const { save } = require("../save_json");
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
+let text = db.collection('text')
 
 
 router.get("/", async (req, res, next) => {
-  let my_file = await s3
-  .getObject({
-    Bucket: "cyclic-cooperative-sun-hat-bat-eu-west-3",
-    Key: "content.json",
-  })
-  .promise();
-const Content = JSON.parse(my_file.Body)?.Content;
-console.log("Get: ", Content);
-  res.json({
-    status: "success",
-    result: Content,
-  });
+  let list = await text.list();
+  res.send(list);
 });
 
 router.post("/", async (req, res, next) => {
-  const { Content } = req.body;
-  console.log("Post: ", Content);
-  content.Content = Content;
-  save(content);
-  res.json({
-    status: "success",
-    result: Content,
-  });
+  const {content } = req.body;
+  await text.set(content)
+  res.end();
 });
 
 module.exports = router;
